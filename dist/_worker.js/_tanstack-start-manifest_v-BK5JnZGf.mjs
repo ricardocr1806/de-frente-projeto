0edcae33fc0ec101dc801ead155e6bd1d91f62cd
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-04 20:10:13 -0300

    deploy: push built dist Heart-Bridge-Site

---
+++ dist/_worker.js/_tanstack-start-manifest_v-BK5JnZGf.mjs
@@ -0,0 +1,25 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-BK5JnZGf.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/__root.tsx",
+		children: ["/", "/obrigado"],
+		preloads: ["/assets/index-BY3Dj5Ek.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-BY3Dj5Ek.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-DJhU22_P.js"]
+	},
+	"/obrigado": {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/obrigado.tsx",
+		children: void 0,
+		preloads: ["/assets/obrigado-BeOVtpnz.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
