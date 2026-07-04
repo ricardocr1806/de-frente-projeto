77fce87160b1470714edff2952cee945a3b1fb77
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-04 20:10:11 -0300

    deploy: push built dist Heart-Bridge-Site

---
+++ dist/_worker.js/_ssr/speaker-opt-C3KrRL-K.mjs
@@ -0,0 +1,4 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/speaker-opt-C3KrRL-K.js
+var speaker_opt_default = "/assets/speaker-opt-BuJb6J0V.jpg";
+//#endregion
+export { speaker_opt_default as t };
