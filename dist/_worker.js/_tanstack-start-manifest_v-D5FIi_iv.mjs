ec6bf2266d8664f39aa8863f3fe80d16a05772ca
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-05 08:39:11 -0300

    fix: preco 9.90 em todos os lugares

---
+++ dist/_worker.js/_tanstack-start-manifest_v-D5FIi_iv.mjs
@@ -0,0 +1,25 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-D5FIi_iv.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/__root.tsx",
+		children: ["/", "/obrigado"],
+		preloads: ["/assets/index-DLg_W-XO.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-DLg_W-XO.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-jynR8gUQ.js"]
+	},
+	"/obrigado": {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/obrigado.tsx",
+		children: void 0,
+		preloads: ["/assets/obrigado-WPp0z5eq.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
