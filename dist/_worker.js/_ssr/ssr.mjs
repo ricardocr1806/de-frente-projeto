68251422799331f554a5e3c43369a883c17e6022
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-05 08:39:09 -0300

    fix: preco 9.90 em todos os lugares

--- dist/_worker.js/_ssr/ssr.mjs
+++ dist/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-ClU9vppM.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-V0nhSlpH.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
