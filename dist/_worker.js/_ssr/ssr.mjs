5a4def783d331a8331710c03c93bd9d73e195697
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-05 08:31:50 -0300

    fix: foto mobile menor + preco 9.90

--- dist/_worker.js/_ssr/ssr.mjs
+++ dist/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-CZ6kOi67.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-ClU9vppM.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
