9d965efed5cfc733d13228f90d5be53ccc975658
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-05 08:43:14 -0300

    fix: preco grande FinalCTA 9.90

---
+++ dist/_worker.js/_tanstack-start-manifest_v-CzwpBFtn.mjs
@@ -0,0 +1,25 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CzwpBFtn.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/__root.tsx",
+		children: ["/", "/obrigado"],
+		preloads: ["/assets/index-g9spN0K1.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-g9spN0K1.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-D_i6lSZ6.js"]
+	},
+	"/obrigado": {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/obrigado.tsx",
+		children: void 0,
+		preloads: ["/assets/obrigado-jNL4Vh1s.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
