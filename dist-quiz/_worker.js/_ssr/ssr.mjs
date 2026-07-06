0471af1995ef2dbf5a86fb2dea9cb63e38a7796e
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 18:36:39 -0300

    feat: quiz v2 tema suave claro

--- dist-quiz/_worker.js/_ssr/ssr.mjs
+++ dist-quiz/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-D6ciiMcb.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-VUA04n4z.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
