42a87e80f9bbc67dceeea0a87ffc224935b2c2ea
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 16:52:09 -0300

    feat: quiz v2 tema verde/ouro funil familiar

--- dist-quiz/_worker.js/_ssr/ssr.mjs
+++ dist-quiz/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-B-CPU2BC.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-D6ciiMcb.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
