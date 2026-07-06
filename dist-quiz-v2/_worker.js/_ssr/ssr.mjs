88fee78e4735023a699ccc52ba226251e7531453
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 15:02:02 -0300

    feat: rota /v2 adicionada ao quiz

--- dist-quiz-v2/_worker.js/_ssr/ssr.mjs
+++ dist-quiz-v2/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-C_kk9Fe7.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-BN3xpelu.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
