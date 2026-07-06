846a5faded1a6158910339edccb8905bab6515b5
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 15:16:27 -0300

    feat: rota /v2 adicionada ao quiz original

---
+++ dist-quiz/_worker.js/_tanstack-start-manifest_v-hP1CIc00.mjs
@@ -0,0 +1,34 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-hP1CIc00.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/__root.tsx",
+		children: [
+			"/",
+			"/dashboard",
+			"/v2"
+		],
+		preloads: ["/assets/index-DL_1sDbB.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-DL_1sDbB.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-BxDMwndz.js", "/assets/PostResultFlow-9U3rDPBJ.js"]
+	},
+	"/dashboard": {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/dashboard.tsx",
+		children: void 0,
+		preloads: ["/assets/dashboard-rSGKlMc_.js"]
+	},
+	"/v2": {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/v2.tsx",
+		children: void 0,
+		preloads: ["/assets/v2-BxDMwndz.js", "/assets/PostResultFlow-9U3rDPBJ.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
