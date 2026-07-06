afea5673e735e4e0db952888ce22c5dd1bf23494
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 20:20:29 -0300

    feat: quiz nova foto Pierry + datas 15 e 16 de julho

---
+++ dist-quiz/_worker.js/_tanstack-start-manifest_v-VMd-6z8w.mjs
@@ -0,0 +1,34 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-VMd-6z8w.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/__root.tsx",
+		children: [
+			"/",
+			"/dashboard",
+			"/v2"
+		],
+		preloads: ["/assets/index-D66lwSS4.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-D66lwSS4.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-DQIiv-wK.js", "/assets/PostResultFlow-CX95hf-f.js"]
+	},
+	"/dashboard": {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/dashboard.tsx",
+		children: void 0,
+		preloads: ["/assets/dashboard-DvarDxrC.js"]
+	},
+	"/v2": {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/v2.tsx",
+		children: void 0,
+		preloads: ["/assets/v2-CZwSipOC.js", "/assets/PostResultFlow-CX95hf-f.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
