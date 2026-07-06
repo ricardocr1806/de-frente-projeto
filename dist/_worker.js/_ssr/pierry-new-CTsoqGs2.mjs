b3a64d6a3a11be96589ae11282d16c7363138664
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 19:41:11 -0300

    feat: nova foto Pierry, datas 15/16 julho, info perto do botão

---
+++ dist/_worker.js/_ssr/pierry-new-CTsoqGs2.mjs
@@ -0,0 +1,4 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/pierry-new-CTsoqGs2.js
+var pierry_new_default = "/assets/pierry-new-WI_UCmAX.jpg";
+//#endregion
+export { pierry_new_default as t };
