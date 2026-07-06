b5c429bc39722fa4e99375076749c0a0f29746dc
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 13:47:21 -0300

    feat: quiz-pierry-v2 initial deploy

---
+++ dist-quiz-v2/_worker.js/_tanstack-start-manifest_v-Bco5ygn-.mjs
@@ -0,0 +1,25 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-Bco5ygn-.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/__root.tsx",
+		children: ["/", "/dashboard"],
+		preloads: ["/assets/index-CCb3ToYe.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CCb3ToYe.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-Bbg9snl1.js"]
+	},
+	"/dashboard": {
+		filePath: "/Users/ricardocastro/Documents/Quiz-Pierry-Site/src/routes/dashboard.tsx",
+		children: void 0,
+		preloads: ["/assets/dashboard-CpGoGunc.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
