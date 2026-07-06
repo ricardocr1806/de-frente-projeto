965a52f480c8768a0cf29eea0d811df7963eba2f
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 20:13:43 -0300

    feat: nova foto Pierry + datas 15 e 16 de julho

--- dist/_worker.js/_ssr/ssr.mjs
+++ dist/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-ZceBvuaq.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-DiBufNlr.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
