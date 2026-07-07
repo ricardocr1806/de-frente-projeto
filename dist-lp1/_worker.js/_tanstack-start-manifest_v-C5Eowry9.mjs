210632553cec1069ae9fc5a52555702d1b3f14eb
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-07 11:39:12 -0300

    feat: datas 13 a 16 de julho LP1+LP2

---
+++ dist-lp1/_worker.js/_tanstack-start-manifest_v-C5Eowry9.mjs
@@ -0,0 +1,34 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-C5Eowry9.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ricardocastro/Documents/LP-Pierry-Site/src/routes/__root.tsx",
+		children: [
+			"/",
+			"/obrigado",
+			"/privacidade"
+		],
+		preloads: ["/assets/index-CS4Giu6h.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CS4Giu6h.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ricardocastro/Documents/LP-Pierry-Site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-Bpet5Izc.js"]
+	},
+	"/obrigado": {
+		filePath: "/Users/ricardocastro/Documents/LP-Pierry-Site/src/routes/obrigado.tsx",
+		children: void 0,
+		preloads: ["/assets/obrigado-Ckb-akEg.js"]
+	},
+	"/privacidade": {
+		filePath: "/Users/ricardocastro/Documents/LP-Pierry-Site/src/routes/privacidade.tsx",
+		children: void 0,
+		preloads: ["/assets/privacidade-BKYYyRln.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
