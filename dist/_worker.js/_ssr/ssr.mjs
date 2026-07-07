0f3a39924cdcd3eb5bc85dd473dfa0bf4db41cde
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 22:22:35 -0300

    feat: LP2 mobile hero foto igual ao desktop

--- dist/_worker.js/_ssr/ssr.mjs
+++ dist/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-DvmMe9fa.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-BvL7ODG0.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
