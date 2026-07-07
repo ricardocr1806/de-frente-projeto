7103b7654175791f462f3b45ca0992d881f18717
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-07 14:29:32 -0300

    feat: LP2 checkout 1d926e + preço R$ 19,90

---
+++ dist/_worker.js/_tanstack-start-manifest_v-D8RmNGS7.mjs
@@ -0,0 +1,25 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-D8RmNGS7.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/__root.tsx",
+		children: ["/", "/obrigado"],
+		preloads: ["/assets/index-CzYXgidB.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CzYXgidB.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-8yFQfWfV.js"]
+	},
+	"/obrigado": {
+		filePath: "/Users/ricardocastro/Documents/Heart-Bridge-Site/src/routes/obrigado.tsx",
+		children: void 0,
+		preloads: ["/assets/obrigado-B3bWh8j3.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
