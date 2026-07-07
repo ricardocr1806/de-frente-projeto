7c5cde46705e601070402284ee1709f7ef485bfe
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-07 11:39:10 -0300

    feat: datas 13 a 16 de julho LP1+LP2

--- dist-lp1/_worker.js/_ssr/ssr.mjs
+++ dist-lp1/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-BQVLepr1.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-BGmAe-Zi.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
