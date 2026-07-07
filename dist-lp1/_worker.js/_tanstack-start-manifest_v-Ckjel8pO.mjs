91c045c537eae92220006d209d4c9fd8ec4bf87c
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 23:38:54 -0300

    feat: LP1 obrigado novo link grupo WhatsApp

---
+++ dist-lp1/_worker.js/_tanstack-start-manifest_v-Ckjel8pO.mjs
@@ -0,0 +1,34 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-Ckjel8pO.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ricardocastro/Documents/LP-Pierry-Site/src/routes/__root.tsx",
+		children: [
+			"/",
+			"/obrigado",
+			"/privacidade"
+		],
+		preloads: ["/assets/index-Buq2iMQ1.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-Buq2iMQ1.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ricardocastro/Documents/LP-Pierry-Site/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-qNwFymn2.js"]
+	},
+	"/obrigado": {
+		filePath: "/Users/ricardocastro/Documents/LP-Pierry-Site/src/routes/obrigado.tsx",
+		children: void 0,
+		preloads: ["/assets/obrigado-UgUEA7bO.js"]
+	},
+	"/privacidade": {
+		filePath: "/Users/ricardocastro/Documents/LP-Pierry-Site/src/routes/privacidade.tsx",
+		children: void 0,
+		preloads: ["/assets/privacidade-zmNY1-LX.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
