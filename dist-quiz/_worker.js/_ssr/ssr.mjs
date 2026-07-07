38f4d2cb6fd03def9d018a4892d5af43c52c96a8
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-06 21:57:02 -0300

    fix: quiz UTMs no checkout + preço R$ 9,90

--- dist-quiz/_worker.js/_ssr/ssr.mjs
+++ dist-quiz/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-B0cQOsJ5.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-WFn_ewVC.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
