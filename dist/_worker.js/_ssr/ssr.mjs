1c9131d3d0e4668b10251bcd4563c046bd2d103f
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-08 12:40:46 -0300

    feat: /v27 tema claro + quatro dias em ambas as rotas

--- dist/_worker.js/_ssr/ssr.mjs
+++ dist/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-Da_R9vqv.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-D_9bSzhF.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
