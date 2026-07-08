488b31379d53f379316875c66e205a682d3f7f29
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-07 21:35:21 -0300

    fix: v27 sobrescreve variaveis CSS para tema claro correto

--- dist/_worker.js/_ssr/ssr.mjs
+++ dist/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-B9XPL6Lp.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-BdM-Ot-k.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
