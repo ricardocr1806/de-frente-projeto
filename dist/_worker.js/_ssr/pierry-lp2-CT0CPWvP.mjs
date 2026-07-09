cd060885ae080c7996e5f7787243933c7db05dc9
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-09 12:23:02 -0300

    deploy: update dist/_worker.js/_ssr/pierry-lp2-CT0CPWvP.mjs

---
+++ dist/_worker.js/_ssr/pierry-lp2-CT0CPWvP.mjs
@@ -0,0 +1,4 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/pierry-lp2-CT0CPWvP.js
+var pierry_lp2_default = "/assets/pierry-lp2-BjQjrymJ.jpg";
+//#endregion
+export { pierry_lp2_default as t };
