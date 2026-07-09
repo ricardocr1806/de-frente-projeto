691449eec4149597702f4d4c8e3a18c47b13d876
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-07-09 10:33:33 -0300

    deploy: update dist/_worker.js/_ssr/ssr.mjs

--- dist/_worker.js/_ssr/ssr.mjs
+++ dist/_worker.js/_ssr/ssr.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 }
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./server-Cq1tlAGz.mjs").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./server-_GV2532r.mjs").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
